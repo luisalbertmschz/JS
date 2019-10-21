29c9749b62ce3e6a3aa091eaf9901d0cf3e48019
Albert <cerebrito07@hotmail.com>
2019-10-21 00:45:59 -0400

    A;adiendo validacion al Ejercio 01 para solo permitir numeros mayores a cero y solo numeros

--- Ejercicios/Ejercicio-01.js
+++ Ejercicios/Ejercicio-01.js
@@ -12,6 +12,16 @@ var numero1 = parseInt(prompt("Ingrese el primer numero",0));
 
 var numero2 = parseInt(prompt("Ingrese el segundo numero",0));
 
+while( 
+    
+ numero1 <= 0 || numero2 <=0 ||  isNaN(numero1) || isNaN(numero2)
+){
+
+
+ numero1 = parseInt(prompt("Ingrese el primer numero", 0));
+
+ numero2 = parseInt(prompt("Ingrese el segundo numero", 0));
+}
 
 
 if( numero1 > numero2){
