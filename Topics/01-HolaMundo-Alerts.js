6f562cbbd7fdad78a5cdde618ddfedbf6aed9f07
Albert <cerebrito07@hotmail.com>
2019-10-21 08:35:53 -0400

    Añadiendo archivos  faltantes

---
+++ Topics/01-HolaMundo-Alerts.js
@@ -0,0 +1,3 @@
+alert("Bienvenido al blog de Host Seven");
+alert("Si tienes alguna duda no dudes e contactarnos");
+document.write("Hola mundo desde fichero externo");
---
+++ Topics/04-Constates.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Constantes
+//Un contenedor de datos como una variable pero cuyo contenido nunca va a variar 
+
+
+var web = "Kirkland Sparkling";
+const sabor = "Black Raspberry";
+
+web = "Sparkling";
+sabor = "Fresa";
+console.log(web);
+
+
