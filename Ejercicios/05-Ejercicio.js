2b84afe79704961bf7871f1ef8b305f32c59cc7e
Albert <cerebrito07@hotmail.com>
2019-10-28 00:15:26 -0400

    Agregando la solución al ejercicio #5

--- Ejercicios/05-Ejercicio.js
+++ Ejercicios/05-Ejercicio.js
@@ -8,3 +8,16 @@
 Mostrar todos los numeros divisores de un n[umero instroducido por el usuario
 
 */
+
+var numero = parseInt(prompt("Ingresa por favor un numero", 1));
+
+
+for ( var i = 0; i <= numero; i++) {
+    
+    if ( numero % i == 0 ) {
+
+        console.log ("Divisor: "+i)
+        
+    }
+    
+}
